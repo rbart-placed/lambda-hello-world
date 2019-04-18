f2841dc0ca722377423cc7775a2dd24159657077
Robert Bart <rbart@placed.com>
2019-04-18 16:40:27 -0700

    increase sleep delay.

--- src/main/java/com/sewichi/dev/HelloWorldHandler.java
+++ src/main/java/com/sewichi/dev/HelloWorldHandler.java
@@ -11,7 +11,7 @@ public class HelloWorldHandler implements RequestHandler<String, String> {
     public String handleRequest(String s, Context context) {
 		try {
 			Random random = new Random();
-			int randomSleepMs = random.nextInt((10000 - 5000) + 1) + 5000;
+			int randomSleepMs = random.nextInt((60000 - 5000) + 1) + 5000;
 			Thread.sleep(randomSleepMs);
 			return String.format("Hello, %s!", s);
 		} catch (InterruptedException e) {
