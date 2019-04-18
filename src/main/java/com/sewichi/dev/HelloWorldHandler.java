5605b5ca5bfaa55ddb9de9d3fcfc95392eebbaa8
Robert Bart <rbart@placed.com>
2019-04-18 16:23:13 -0700

    Sleep for random delay

--- src/main/java/com/sewichi/dev/HelloWorldHandler.java
+++ src/main/java/com/sewichi/dev/HelloWorldHandler.java
@@ -3,10 +3,19 @@ package com.sewichi.dev;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
+import java.util.Random;
+
 public class HelloWorldHandler implements RequestHandler<String, String> {
 
     @Override
     public String handleRequest(String s, Context context) {
-        return String.format("Hello, %s!", s);
-    }
+		try {
+			Random random = new Random();
+			int randomSleepMs = random.nextInt((10000 - 5000) + 1) + 5000;
+			Thread.sleep(randomSleepMs);
+			return String.format("Hello, %s!", s);
+		} catch (InterruptedException e) {
+			return "InterruptedException " + e.getMessage();
+		}
+	}
 }
